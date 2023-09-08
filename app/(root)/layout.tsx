fa4414ceaa3ee56a84140dc5d5bdf02dc76893f0
svartse <killbillbeatrixx@gmail.com>
2023-09-08 23:42:29 +0300

    components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -19,7 +19,19 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${inter.className} bg-dark-1`}>
-          {children}
+          <Topbar />
+          <main>
+            <LeftSidebar />
+
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>
+                {children}
+              </div>
+            </section>
+
+            <RightSidebar />
+          </main>
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
