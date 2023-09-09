b482bbeac224858bef35d5d408408779d5f12a23
svartse <killbillbeatrixx@gmail.com>
2023-09-09 14:43:35 +0300

    Topbar

---
+++ components/shared/Topbar.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import Image from "next/image"
+import { SignedIn } from "@clerk/nextjs"
+
+function Topbar(){
+    const isUserLoggedIn = true;
+
+    return (
+    <nav className="topbar">
+        <Link href="/" className="flex items-center gap-4">
+            <Image src="/assets/logo.svg" alt="logo" width={28} height={28}/>
+            <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+        </Link>
+        <div className="flex items-center gap-1">
+            <div className="block md:hidden">
+                <SignedIn></SignedIn>
+
+            </div>
+        </div>
+    </nav>
+    )
+}
+
+export default Topbar
