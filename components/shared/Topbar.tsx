7879131f9602111aec5257d1b737781630c04fdc
svartse <killbillbeatrixx@gmail.com>
2023-09-11 23:36:31 +0300

    Topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link"
 import Image from "next/image"
-import { SignedIn } from "@clerk/nextjs"
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs"
 
 function Topbar(){
-    const isUserLoggedIn = true;
-
     return (
     <nav className="topbar">
         <Link href="/" className="flex items-center gap-4">
@@ -13,9 +11,26 @@ function Topbar(){
         </Link>
         <div className="flex items-center gap-1">
             <div className="block md:hidden">
-                <SignedIn></SignedIn>
-
+                <SignedIn>
+                    <SignOutButton>
+                        <div className="flex cursor-pointer">
+                            <Image
+                            src='/assets/logout.svg'
+                            alt='logout'
+                            width={24}
+                            height={24}
+                            />
+                        </div>
+                    </SignOutButton>
+                </SignedIn>
             </div>
+            <OrganizationSwitcher 
+                appearance={{
+                    elements: {
+                        organizationSwitcherTrigger: 'py-2 px-4 '
+                    }
+                }}
+            />
         </div>
     </nav>
     )
